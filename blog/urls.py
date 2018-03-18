f8fef76ef1739af42a2cdd5db8d34c33d0ca7ec9
Alefe Fazio <diegoalefe@gmail.com>
2018-03-18 16:28:41 -0300

    Add views to post detail, create new routes and modified html

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import include, re_path
 from . import views
 urlpatterns = [
     re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
